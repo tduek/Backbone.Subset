d424cd2240674f9db02f5315effdbed2d0e84d8a
Tommy Duek <taduek@gmail.com>
2014-10-22 14:13:40 -0400

    Behave just like like Backbone.Collection.

--- backbone.subset.js
+++ backbone.subset.js
@@ -3,12 +3,19 @@
     options || (options = {});
     if (options.parentCollection instanceof Backbone.Collection) {
       this.parentCollection = options.parentCollection;
-      this.model = this.parentCollection.model;
+      if (options.model) {
+        this.model = options.model;
+      }
+      else if (
+        this.model === Backbone.Model &&
+        this.parentCollection.model !== Backbone.Model
+      ) {
+        this.model = this.parentCollection.model;
+      }
     } else {
       throw 'ArgumentError: Must supply an instanceof Backbone.Collection as parentCollection';
     }
-
-    if (options.comparator) this.comparator = options.comparator;
+    if (options.comparator !== void 0) this.comparator = options.comparator;
     this._reset();
     this.initialize.apply(this, arguments);
     if (models) this.add(models, _.extend({silent: true}, options));
@@ -17,30 +24,47 @@
   Subset.extend = Backbone.Collection.extend;
   Subset.prototype = Object.create(Backbone.Collection.prototype);
 
-  Subset.prototype._prepareModel = function(attrs, options) {
-    var existingInParent, id, tagetModel, parent;
-    parent = this.parentCollection, targetModel = this.model;
+  _.extend(Subset.prototype, {
+    constructor: Subset,
 
-    id = attrs[targetModel.prototype.idAttribute || 'id'];
-    if (existingInParent = this.parentCollection.get(id)) {
-      return existingInParent;
+    _prepareModel: function(attrs, options) {
+      var existingInParent, id, tagetModel, parent;
+      parent = this.parentCollection, targetModel = this.model;
+
+      if (attrs) id = this.modelId(attrs);
+      if (existingInParent = (id && this.parentCollection.get(id))) {
+        attrs = (this._isModel(attrs)) ? attrs.attributes : attrs;
+        if (options.parse) attrs = existingInParent.parse(attrs);
+        existingInParent.set(attrs, options);
+        return existingInParent;
+      }
+
+      if (attrs instanceof Backbone.Model) {
+        if (!attrs.collection) attrs.collection = this;
+        parent.add(attrs);
+        return attrs;
+      }
+
+      options = options ? _.clone(options) : {};
+      options.collection = this;
+      var model = new targetModel(attrs, options);
+      if (!model.validationError) {
+        model.collection = this;
+        parent.add(model);
+        return model;
+      }
+
+      this.trigger('invalid', this, model.validationError, options);
+      return false;
+    },
+
+    clone: function () {
+      return new this.constructor(this.models, {
+        model: this.model,
+        comparator: this.comparator,
+        parentCollection: this.parentCollection
+      });
     }
-
-    if (attrs instanceof Backbone.Model) {
-      parent.add(attrs);
-      return attrs;
-    }
-
-    options = options ? _.clone(options) : {};
-    options.collection = this;
-    var model = new targetModel(attrs, options);
-    if (!model.validationError) {
-      parent.add(model);
-      return model;
-    }
-
-    this.trigger('invalid', this, model.validationError, options);
-    return false;
-  };
+  });
 
 })();
