78614115a221d3cc0e9ccca71315365fbae10ff1
Tommy Duek <taduek@gmail.com>
2014-10-23 09:49:51 -0400

    Start testing subset.

--- test/subset.js
+++ test/subset.js
@@ -1,19 +1,40 @@
 (function() {
 
-  var a, b, c, d, e, col, otherCol;
+  var model, col, sub;
 
   module("Backbone.Subset", {
-
-    setup: function() {
-      a         = new Backbone.Model({id: 3, label: 'a'});
-      b         = new Backbone.Model({id: 2, label: 'b'});
-      c         = new Backbone.Model({id: 1, label: 'c'});
-      d         = new Backbone.Model({id: 0, label: 'd'});
-      e         = null;
-      col       = new Backbone.Collection([a,b,c,d]);
-      otherCol  = new Backbone.Collection();
+    setup: function () {
+      model = new Backbone.Model({id: 1, prop: 'value'});
+      col = new Backbone.Collection;
+      sub = new Backbone.Subset([], {parentCollection: col});
     }
+  });
 
+  test('`add` adds models to parentCollection', 2, function () {
+    sub.add(model);
+
+    equal(col.length, 1);
+    equal(col.first(), model);
+  });
+
+  test('takes model from parent if parent has it', 2, function () {
+    col.add(model);
+    var newModel = new Backbone.Model(model.attributes);
+    sub.add(newModel);
+
+    equal(col.first(), sub.first());
+    ok(!newModel.collection)
+  });
+
+  test('inherits `url`, `model`, and `comparator` from parent if one isnt defined', function () {
+    col.model = Backbone.Model.extend();
+    col.url = '/test';
+    col.comparator = 'id';
+    var sub = new Backbone.Subset([], {parentCollection: col});
+
+    equal(col.url, sub.url);
+    equal(col.model, sub.model);
+    equal(col.comparator, sub.comparator);
   });
 
 
